6e14ee56068cfa656e4ae68bb2393d07b4db0193
Hemant <hrsmart2150@gmail.com>
2022-08-23 12:50:48 +0530

    Saw js strings

---
+++ strings.js
@@ -0,0 +1,15 @@
+let name = "Harry";
+console.log(name.replace('rr','pp'))
+
+let fruit = "    Apple    " 
+console.log(fruit.trim())// it removes only space from the beginning and end of the string not from the middle
+
+let str1 = "Harry";
+let str2 = "Shubh";
+console.log(str1.concat(" and ", str2, " are good friends"))
+
+// QuickQuiz
+let future = "Bright"
+for(let i = 0; i < future.length; i++){
+    console.log(future[i])
+}
