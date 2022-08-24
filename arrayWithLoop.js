11f5b9dcc6b9a5acb4fda82e0c20d989f138849e
Hemant <hrsmart2150@gmail.com>
2022-08-24 11:22:52 +0530

    Saw for of and in loops with array

---
+++ arrayWithLoop.js
@@ -0,0 +1,25 @@
+// let num = [1, 2, 3, 4]
+// num.forEach((element) => console.log(element * element)) // doesnt affect the original array
+// console.log(num)
+
+// we would be getting HTML Collection in web apps so thats why we have convert it into array
+// let HTMLCollection = [] // HTML collection is not an array we have to covert it into array using Array.from() method
+// Array.from(HTMLCollection) // converts HTML collection to an array
+
+// lets take an example
+// let name = "Harry"
+// let arr = Array.from(name)
+// console.log(arr)
+
+
+// for of loop -> return the value of array
+// let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// for (let a of num) {
+//     console.log(a)
+// }
+
+// for of loop -> returns the index of array
+// let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// for (let a in num) {
+//     console.log(a)
+// }
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+he index
+let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for (let a in num) {
+    console.log(a)
+}
